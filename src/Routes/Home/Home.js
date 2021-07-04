3d5779a5adea55abfad9c4f58f252e4c473ce88d
prodhc <zionywood@gmail.com>
2021-07-03 21:19:28 -0700

    home v2 done

--- src/Routes/Home/Home.js
+++ src/Routes/Home/Home.js
@@ -1,8 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import './Home.scss'
-import Main from '../../resources/home_main.jpg'
-import Main2 from '../../resources/home_main2.jpg'
-import Main3 from '../../resources/home_main3.jpg'
+import flatArt from '../../resources/fhome_flatart.png'
 import Second from '../../resources/home_second.jpg'
 
 import Header from '../crossRouteComponents/Header'
@@ -10,52 +8,8 @@ import Footer from '../crossRouteComponents/Footer'
 
 const Home = () => {
 
-    const initalData = Main;
 
-    const [mainImage, setMainImage] = useState(initalData)
-
-    useEffect(() => {
-      let sheet = document.styleSheets[0]
-      if (mainImage === Main) {
-        setTimeout(() => {
-
-          
-          // sheet.insertRule("@keyframes {}")
-          setMainImage(Main2)
-          document.getElementById("image2").style.animation = "fadeInPic 2s";
-          
-          setTimeout(() => {document.getElementById("image2").style.removeProperty("animation");}, 2500)
-          setTimeout(() => {document.getElementById("image2").style.animation = "fadeOutPic 2s";}, 4500)
-          // setTimeout(() => {document.getElementById("image2").style.removeProperty("animation");}, 6000)
-
-
-          // document.getElementById("image2").style[] = "opacity 0s 1s ease-in"
-          
-          // document.getElementById("image2").style.transition = "opacity 2s ease-in"
-          // document.getElementById("image2").style.opacity = "1"
-          // document.getElementById("image2").style.removeProperty("animation")
-        }, 5000)
-      } else if (mainImage === Main2) {
-        setTimeout(() => {
-          setMainImage(Main3)
-          document.getElementById("image3").style.animation = "fadeInPic 2s";
-          
-          
-          
-          setTimeout(() => {document.getElementById("image3").style.removeProperty("animation");}, 2500)
-          setTimeout(() => {document.getElementById("image3").style.animation = "fadeOutPic 2s";}, 4500)
-        }, 5000)
-      } else if (mainImage === Main3) {
-        setTimeout(() => {
-          setMainImage(Main)
-          document.getElementById("image1").style.animation = "fadeInPic 2s";
-          
-          setTimeout(() => {document.getElementById("image1").style.removeProperty("animation");}, 2500)
-          setTimeout(() => {document.getElementById("image1").style.animation = "fadeOutPic 2s";}, 4500)
-        }, 5000)
-      }
-
-    })
+   
     
     return (
       <>
@@ -64,23 +18,19 @@ const Home = () => {
        
       <div class="topHomeContainer">
 
-        {
-        mainImage === Main ? <img key={Date.now} class="topImage" id="image1" src={Main} alt="Real Airpods"/> 
-        : mainImage === Main2 ? <img key={Date.now} class="topImage" id="image2" src={Main2} alt="Real Computer"/>
-        : mainImage === Main3 ? <img key={Date.now} class="topImage" id="image3" src={Main3} alt="Real Fashion"/>
-        : console.log("error")
-      }
-
+       
+        <img src={flatArt} class="topImage"/>
 
           <div class="topHomeTextDiv">
-
+{/* 
             {
               mainImage === Main ? <h2><span class="topHomeTextSecColor">REAL</span> People. <span class="topHomeTextSecColor">REAL</span> Products.</h2>
               : mainImage === Main2 ?  <h2><span class="topHomeTextSecColor">REAL</span> People. <span class="topHomeTextSecColor">REAL</span> Electronics.</h2>
               : mainImage === Main3 ?  <h2><span class="topHomeTextSecColor">REAL</span> People. <span class="topHomeTextSecColor">REAL</span> Fashion.</h2>
               : console.log("error")
-            }
-    
+            } */}
+            <h2>Real <span>People.</span><br/> Real <span>Products.</span></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio facilisis mauris sit amet massa.</p>
             <button class="topHomeButton">EXPERIENCE <span class="topHomeTextSecColor">REAL</span> SHOPPING</button>
           </div>
       </div>
